5588168e61f0807fec057d75f97b6218d73508d8
alamin <alaminakash9754@gmail.com>
2024-03-29 19:40:23 +0600

    single product get from server

--- index.js
+++ index.js
@@ -48,7 +48,8 @@ async function run() {
 
     app.get('/products/:id', async(req, res) => {
         const id = req.params.id;
-        const query = {_id: new ObjectId(id)};
+        console.log(id)
+        const query = {_id: id};
         const result = await productCollection.findOne(query);
         console.log(result);
         res.send(result);
